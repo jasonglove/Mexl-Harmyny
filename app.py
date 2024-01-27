0d65de77cb02e1d2858d3a25d47a38a1060e17de
Samuel Lackey <34922533+slackey14@users.noreply.github.com>
2024-01-27 14:50:10 -0600

    quiz basics, session variable

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ from io import BytesIO
 import subprocess
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/save_quiz_answers', methods=['POST']) #Saves after quiz
+def save_quiz_answers():
+    #Logic to save quiz answers
+    if request.method == 'POST':
+        #Retrieve the quiz answers from the form data
+        fat = request.form.get('fat')
+        #Add more answers as needed
+        
+        #Save the quiz answers in session variables
+        session['fat_amount'] = fat
+        
+        #Redirect to the homepage after saving the answers
+        return redirect(url_for('index'))
+
+    return redirect(url_for('index'))  #Redirect after saving answers
 
 @app.route('/recipe')
 def quiz():
