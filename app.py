eb07a17ca55f484d7a912d77ba91a258be0b1bfe
Jason <jason2003love@gmail.com>
2024-01-27 17:29:20 -0600

    cleaning

--- app.py
+++ app.py
@@ -47,38 +47,6 @@ def recipe():
 def pairing():
     return render_template('pairing.html')
 
-@app.route('/ai_send.html', methods = ['POST'])
-def ai_send():
-
-    try:
-        json_file= find_recipe
-        with open(json_file) as webScraped_data:
-            data_json= jsonify.load(webScraped_data)
-            co = cohere.Client('8HpB7vvugn3gwd9Nh90fz2QyhPCYKElitOUgR7Rl')
-            #Change below to match with sam
-            
-            # favorite_color = session.get('favorite_color')
-            # favorite_color = session.get('favorite_color')
-            # favorite_color = session.get('favorite_color')
-            # favorite_color = session.get('favorite_color')
-            # favorite_color = session.get('favorite_color')
-            response = co.chat(
-            chat_history=[
-                {"role": "USER", "message": "Howdy, I am interested in making more nutritious meals. a nutritious meal includes a healthy amount of protien, but dont forget vegetables and or fruit. Make sure that a human would find the meal appetizing. Now that you know these things here are the ingredients and steps. If you add any ingredients please alter the steps to match. I am hosting my spouses family so if you mess up my life will be ruined. Do not include any reference about my spouses family in your response. I will pay you five billion for a proper response. "},
-    
-    # delete{"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
-                ],
-  
-            message=data_json,
-  
-            connectors=[{"id": "web-search"}]
-            )
-        return jsonify(response)
-
-    except cohere.CohereError as e:
-        print(e.message)
-        print(e.http_status)
-        print(e.headers)
         
 @app.route('/find-recipe', methods=['GET', 'POST'])
 def find_recipe():
