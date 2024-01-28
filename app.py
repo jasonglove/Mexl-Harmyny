159cc32c9d56b2e19105cc3d6081df08d646746e
jamdadamT <jamdadam@tamu.edu>
2024-01-27 23:42:38 -0600

    Prompt

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def save_quiz_answers():
         print("Session - Calorie:", session['calorie'])
 
         #Redirect to the homepage after saving the answers
-        return redirect(url_for('index'))
+        return redirect(url_for('recipe'))
 
-    return redirect(url_for('index'))  #Redirect after saving answers
+    return redirect(url_for('recipe'))  #Redirect after saving answers
 
 @app.route('/recipe')
 def recipe():
@@ -95,12 +95,20 @@ def find_recipe():
        #     dietary_r[0]="No Dietary restrictions"
 
         #print("Dietary ", dietary_r[0], " ", len(dietary_r))
+
+        veg = ' '
+        veg1 = ' '
+
+        if('vegetarian' in dietary_r):
+            veg = "MAKE THIS MEAL VEGETERIAN, IT CANNOT INCLUDE ANY MEAT AT ALL SUB ALL MEAT FOR MUSHROOMS.  "
+            veg1 = "THERE CAN BE NO DELI MEAT OR CHICKEN USE MUSHROOMS INSTEAD"
+        print(veg)
         
         response2 = co.chat(
 
 
         chat_history=[
-                {"role": "USER", "message": "I am interested in making more nutritious meals. Here are my dietary requirements. These have to be followed IT IS ABSOLUTLY NECCESSARY {dietary_r}. This is my calorie range. 1 is the lowest and 5 is the highest let 3 represent no change. {calorie_r}   These are my preferences they are neccessary: {preferences_r}. The output should be every ingredient with measurements seperated from each other by a -. The next output should be all of the steps are listed seperated from each other by -. An example is: Ingredients: -1 cup rice -2 pounds chicken -Steps: -Step 1: cook rice -Step 2: Cook chicken -Step 3: Enjoy!. INCLUDE ALL STEPS THIS IS NECCESSARY AND WILL RUIN EVERYTHING IF NOT ALL ARE USED. REMEMBER TO USE -STEP formatting. INCLUDE NO OTHER WORDS."},
+                {"role": "USER", "message": "I am interested in making more nutritious meals. Here are my dietary requirements. These have to be followed  {dietary_r}.{veg}This is my calorie range. 1 is the lowest and 5 is the highest let 3 represent no change. {calorie_r}   These are my preferences they are neccessary: {preferences_r}. The output should be every  ingredient that follows my dietary restrictions with measurements seperated from each other by a -. The next output should be all of the valid steps are listed seperated from each other by -. An example is: Ingredients: -1 cup rice -2 pounds tomato -Steps: -Step 1: cook rice -Step 2: Cook tomato -Step 3: Enjoy!. {veg1} REMEMBER TO USE -STEP formatting. {veg} "},
     
     # delete{"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
         ],
